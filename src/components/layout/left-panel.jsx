95ad6c794e8f73a20e79251361f2bd44e0ddff38
daniel bisiriyu <daniel.bisiriyu.o@gmail.com>
2024-02-06 23:03:30 +0100

    Added short description to left-panel component

--- src/components/layout/left-panel.jsx
+++ src/components/layout/left-panel.jsx
@@ -1,5 +1,9 @@
 function LeftPanel() {
-    return <div></div>
+    return <div>
+        <h1>Hi, My name is Daniel, Allow me introduce myself.</h1>
+        <p>A front-end engineer with over four years experience building web application</p>
+        
+    </div>
 } 
 
 export default LeftPanel
