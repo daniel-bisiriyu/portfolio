cced57d131c72e76a41efc098ee5aafc5f8b8927
daniel bisiriyu <daniel.bisiriyu.o@gmail.com>
2024-02-09 23:27:02 +0100

    Added a quick list of projects

--- src/components/layout/right-panel.jsx
+++ src/components/layout/right-panel.jsx
@@ -1,5 +1,19 @@
 function RightPanel() {
-    return <div></div>
+    return <div>
+        <h3>Here are a couple of projects I've worked on!</h3>
+        <ul>
+            <li>Jiggle</li>
+            <li>Classtivities</li>
+            <li>Scelloo</li>
+            <li>YouID</li>
+            <li>Gradely</li>
+            <li>eYouthrescue</li>
+            <li>Morena</li>
+            <li>Moniepoint</li>
+            Personal Project
+            <li>helpmedecide.com</li>
+        </ul>
+    </div>
 } 
 
 export default RightPanel
