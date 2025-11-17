4c2df4485e48bbe430ad0e96b9267f91b11aa9e7
olaleye247 <daniel.bisiriyu.o@gmail.com>
2025-11-17 13:10:19 +0100

    WIP: loading screen

--- app/page.tsx
+++ app/page.tsx
@@ -9,34 +9,33 @@ import Hero from "./components/hero";
 import LoaderText from "./components/loading-text";
 import Navbar from "./components/navbar";
 import Skills from "./components/skills";
-// import { useEffect } from "react";
+import { useEffect } from "react";
 
 import gsap from "gsap";
 
 export default function Home() {
-  // useEffect(() => {
-  //   gsap
-  //     .timeline()
-  //     .to(".loading-screen", {
-  //       yPercent: -100,
-  //       ease: "power4.inOut",
-  //       duration: 1,
-  //     })
-  //     .from(".page-content", {
-  //       opacity: 0,
-  //       y: 50,
-  //       duration: 0.8,
-  //       ease: "power2.out",
-  //     });
-  // }, []);
+  useEffect(() => {
+    gsap
+      .timeline()
+      .to(".loading-screen", {
+        yPercent: -100,
+        ease: "power4.inOut",
+        duration: 1,
+      })
+      .from(".page-content", {
+        opacity: 0,
+        y: 50,
+        duration: 0.8,
+        ease: "power2.out",
+      });
+  }, []);
 
   return (
     <>
       <div className="loading-screen fixed top-0 left-0 w-screen h-screen bg-black z-50 flex items-center justify-center">
-        {/* <h1 className="text-white">Loading...</h1> */}
         <LoaderText />
       </div>
-      {/* <div className={`page-content overflow-hidden`}>
+      <div className={`page-content overflow-hidden`}>
         <CursorTrail />
         <Navbar />
         <Hero />
@@ -45,7 +44,7 @@ export default function Home() {
         <Skills />
         <Contact />
         <Footer />
-      </div> */}
+      </div>
     </>
   );
 }
